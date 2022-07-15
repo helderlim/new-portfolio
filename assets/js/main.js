f526ecff3626d0ff79b37a01bc9bc17dc902315c
helderlim <helderfelima16@gmail.com>
2022-07-14 21:47:19 -0300

    feat: add section portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -96,7 +96,18 @@ modalCloses.forEach((modalClose) => {
 })
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-
+let swiper = new Swiper(".mySwiper", {
+  cssMode: true,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  }
+});
 
 /*==================== TESTIMONIAL ====================*/
 
