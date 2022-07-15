9414c74835bedd60c7fa6f4e64ee972d81137c43
helderlim <helderfelima16@gmail.com>
2022-07-15 13:51:09 -0300

    feat: add section testimonial

--- assets/js/main.js
+++ assets/js/main.js
@@ -96,7 +96,7 @@ modalCloses.forEach((modalClose) => {
 })
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-let swiper = new Swiper(".mySwiper", {
+let swiperPortfolio = new Swiper(".portfolio__container", {
   cssMode: true,
   loop: true,
   navigation: {
@@ -110,7 +110,21 @@ let swiper = new Swiper(".mySwiper", {
 });
 
 /*==================== TESTIMONIAL ====================*/
-
+let swiperTestimonial = new Swiper(".testmonial__container", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 48,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  breakpoints:{
+    568: {
+      slidePerView: 2,
+    }
+  }
+});
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
